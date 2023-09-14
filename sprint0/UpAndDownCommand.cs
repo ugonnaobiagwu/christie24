9a88258552fbfd599656f3e6c70527692ac3a4a0
RGreene37 <144621669+RGreene37@users.noreply.github.com>
2023-09-14 17:32:53 -0400

    Add files via upload

---
+++ sprint0/UpAndDownCommand.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using sprint0;
+using System;
+
+public class UpAndDownCommand : ICommand
+{
+	Sprint0 Game;
+	Texture2D Texture;
+	public UpAndDownCommand(Sprint0 gameObj, Texture2D texture)
+	{
+		Game = gameObj;
+		Texture = texture;
+	}
+
+	public void execute() 
+	{
+		Game.SetSprite(new UpAndDownSprite(Texture, 7, 14));
+	}
+}
---
+++ sprint0/UpAndDownSprite.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Data.Common;
+
+public class UpAndDownSprite: ISprite
+{
+	Texture2D Texture;
+	int Rows;
+	int Columns;
+
+	int SpriteHeight = 0;
+	int SpriteMaxHeight = 10;
+	int SpriteMinHeight = -10;
+	Boolean GoingUp = true;
+	public UpAndDownSprite(Texture2D texture, int rows, int columns)
+	{
+		Texture = texture;
+		Rows = rows;
+		Columns = columns;
+	}
+	public void Update()
+	{
+	
+		if (GoingUp)
+			SpriteHeight++;
+		else
+			SpriteHeight--;
+
+		if (SpriteHeight == SpriteMaxHeight)
+		{
+			GoingUp = false;
+		}else if(SpriteHeight == SpriteMinHeight)
+		{
+			GoingUp = true;
+			
+		}
+	}
+
+	public void Draw(SpriteBatch spriteBatch, int x, int y)
+	{
+		int width = Texture.Width / Columns;
+		int height = Texture.Height / Rows;
+		
+		//POTENTIAL BAD CODE: Hard Coded Numbers
+        Rectangle sheetLocation = new Rectangle(0,1, width, height);
+        Rectangle destinationRectangle = new Rectangle(x, y + SpriteHeight, width, height);
+
+		spriteBatch.Begin();
+		spriteBatch.Draw(Texture, destinationRectangle, sheetLocation, Color.White);
+		spriteBatch.End();
+    }
+}
