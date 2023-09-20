1a280747a2316df16237b39e37e403ada7814444
RGreene37 <144621669+RGreene37@users.noreply.github.com>
2023-09-20 18:09:24 -0400

    Testing to see if adding through Github works

---
+++ sprint0/StandingLinkState.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+
+public class StandingLinkState : ISprite
+{
+	public Texture2D Texture { get; set; }
+	public int Rows { get; set; }
+	public int Cols { get; set; };
+
+	public StandingLinkState(Texture2D texture, int rows, int columns)
+	{
+		Texture = texture;
+		Rows = rows; 
+		Cols = columns;
+
+	}
+    /*POTENTIAL BAD CODE:  This is needed in the main function's update loop because Link's animated sprites need updating.  Thus, ISprite needs an Update() for it to be callable*/
+    public void Update()
+    {
+
+    }
+    public void Draw(SpriteBatch spriteBatch, int x, int y)
+    {
+        /*Figures out where the sprite we want is*/
+        int width = Texture.Width / Columns;
+        int height = Texture.Height / Rows;
+        int xLoc = ;
+        int yLoc = ;
+
+        Rectangle sheetLocation = new Rectangle(width * xLoc, height * yLoc, width, height);
+        Rectangle destinationRectangle = new Rectangle(x, y, width, height);
+
+        /*POTENTIAL BAD CODE: Dr. Kirby mentioned that Begin() and End() before and after each draw is sketchy*/
+        spriteBatch.Begin();
+        spriteBatch.Draw(Texture, destinationRectangle, sheetLocation, Color.White);
+        spriteBatch.End();
+    }
+    
+
+
+}
