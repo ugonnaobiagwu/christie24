1ab6bd277e00c677eea992176417f7fc0a69bc10
kima1810 <kim.7859@osu.edu>
2023-09-27 15:59:26 -0400

    Implemented Commands

---
+++ sprint0/Commands/ResetCommand.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using sprint0;
+
+namespace sprint0.Commands
+{
+    public class ResetCommand : ICommand
+    {
+        Sprint0 Game;
+
+        public ResetCommand(Sprint0 game)
+        {
+            this.Game = game;
+        }
+
+        public void execute()
+        {
+            //Game.Reset();
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ sprint0/Commands/WalkLeftCommand.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using sprint0;
+
+namespace sprint0.Commands
+{
+    public class WalkLeftCommand : ICommand
+    {
+        Sprint0 Game;
+        //Player link
+
+        public WalkLeftCommand(Sprint0 game) //(Player link)
+        {
+            this.Game = game;
+            //this.Link = link;
+            throw new NotImplementedException();
+        }
+
+        public void execute()
+        {
+            //Link.walkLeft();
+            throw new NotImplementedException();
+        }
+    }
+}
