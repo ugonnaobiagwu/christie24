45f20095efc2057bfd21f0522eb560f5e3e6d764
Xing Wu <wu.5650@buckeyemail.osu.edu>
2023-09-28 18:49:26 -0400

    Initial push

---
+++ sprint0/KeyboardMouse&Link/MouseController.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.Xna.Framework.Input;
+using Sprint_2;
+using System;
+using System.Collections.Generic;
+
+public class MouseController : IController
+{
+    ICommand linkAttack;
+    ICommand linkGetDamage;
+
+    private Dictionary<MouseState, ICommand> MouseMap;
+    public KeyboardController()
+    {
+        MouseMap = new Dictionary<int, MouseState>();
+
+    }
+
+    // used to register mouse states with their respective commands
+    public void registerKeys()
+    {
+        // isn't really follwing the name of the method
+        // there is probably an easier way to implement this
+        // no idea how to implement the transition states at the moment
+        linkAttack = new LinkAttack();
+        linkGetDamage = new LinkGetDamage();
+
+    }
+
+    public void Update()
+    {
+        // I have no idea how to put mouse states into an array like keyboard control
+        // so I will just make it so
+        // hopefully this only works while NOTHING is pressed
+
+        MouseState mouseState = Mouse.GetState();
+
+        while (mouseState != null)
+        {
+            if (mouseState.LeftButton == ButtonState.Pressed)
+            {
+                linkAttack.execute();
+            } 
+            else if (mouseState.RightButton == ButtonState.Pressed)
+            {
+                linkGetDamage.execute();
+            }
+        }
+            
+
+        /* this is the code i had previously but I decided to change the code
+        * 
+        * not sure how to implement this but here is the tentative code
+        * 
+        int scrollWheel, horizontalScrollWheel;
+        MouseState mouseState = Mouse.GetState();
+        if (mouseState.LeftButton == ButtonState.Pressed)
+        {
+            // if left mouse button is pressed, it activates attack
+            LinkAttack.execute();
+        }
+        else if (mouseState.RightButton == ButtonState.Pressed)
+        {
+            // if right mouse button is pressed, i have no idea what to implement
+            // perhaps defend for now?
+            // just learned that Link always has a shield. Perhaps use it for something else?
+            LinkDefend.execute();
+        }
+        else if (mouseState.ScrollWheelValue == mouseState.HorizontalScrollWheelValue)
+        {
+            // if it scrolls up, the weapon changes to next weapon
+            LinkChangeNextWeapon.execute();
+        }
+        else if (mouseState.ScrollWheelValue > mouseState.HorizontalScrollWheelValue)
+        { 
+            // if it scrolls down, the weapon changes to previous weapon
+            LinkChangePreviousWeapon.execute();
+        }
+       */
+
+        // I have no idea how the scroll wheel value thing works but i hope this is how you do it
+    }
+}
