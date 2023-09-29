f049b6340e66d34bc4827fa7bc4b8e94ed08f2ed
Ugonna <ugowo@outlook.com>
2023-09-29 16:00:22 -0400

    THE GROUND ITEMS SYTEM IS HERE, [EXPLETIVE] YEAH

--- sprint0/Game1.cs
+++ sprint0/Game1.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using sprint0.Items.groundItems;
 using System.Runtime.CompilerServices;
 
 namespace sprint0
@@ -15,6 +16,7 @@ namespace sprint0
         public int xLoc = 400;
         public int yLoc = 200;
         Texture2D texture;
+        IGroundItemSystem groundItems;
        
         
         //Concrete Commands
@@ -69,7 +71,36 @@ namespace sprint0
 
             TextBox = new TextSprite(font);
 
-
+            //GROUND ITEM SYSTEM STUFF
+            groundItems = new GroundItemSystem(spriteBatch, 200, 200);
+            Texture2D groundBow = Content.Load<Texture2D>("groundItemSprites/groundBow");
+            Texture2D groundBoomerang = Content.Load<Texture2D>("groundItemSprites/groundBoomerang");
+            Texture2D groundBomb = Content.Load<Texture2D>("groundItemSprites/groundBomb");
+            Texture2D groundBlaze = Content.Load<Texture2D>("groundItemSprites/groundBlaze");
+            Texture2D groundHeart = Content.Load<Texture2D>("groundItemSprites/groundHeart");
+            Texture2D groundShimmeringRupee = Content.Load<Texture2D>("groundItemSprites/groundShimmeringRupee");
+            Texture2D groundFairy = Content.Load<Texture2D>("groundItemSprites/groundFairy");
+            Texture2D groundTriforce = Content.Load<Texture2D>("groundItemSprites/groundTriforce");
+            Texture2D groundKey = Content.Load<Texture2D>("groundItemSprites/groundKey");
+            Texture2D groundRupee = Content.Load<Texture2D>("groundItemSprites/groundRupee");
+            Texture2D groundPage = Content.Load<Texture2D>("groundItemSprites/groundPage");
+            Texture2D groundBigHeart = Content.Load<Texture2D>("groundItemSprites/groundBigHeart");
+            Texture2D groundCompass = Content.Load<Texture2D>("groundItemSprites/groundCompass");
+            Texture2D groundClock = Content.Load<Texture2D>("groundItemSprites/groundClock");
+            groundItems.LoadBow(groundBow);
+            groundItems.LoadBoomerang(groundBoomerang);
+            groundItems.LoadBomb(groundBomb);
+            groundItems.LoadBlaze(groundBlaze);
+            groundItems.LoadHeart(groundHeart);
+            groundItems.LoadShimmeringRupee(groundShimmeringRupee);
+            groundItems.LoadFairy(groundFairy);
+            groundItems.LoadTriforcePiece(groundTriforce);
+            groundItems.LoadKey(groundKey);
+            groundItems.LoadRupee(groundRupee);
+            groundItems.LoadPage(groundPage);
+            groundItems.LoadBigHeart(groundBigHeart);
+            groundItems.LoadCompass(groundCompass);
+            groundItems.LoadClock(groundClock);
             // TODO: use this.Content to load your game content here
         }
 
@@ -82,6 +113,7 @@ namespace sprint0
            
             KeyboardCont.Update();
             //Additional Update() added for testing
+            groundItems.Update();
             Luigi.Update();
             
 
@@ -98,6 +130,7 @@ namespace sprint0
             
             //Draws the Textbox
             TextBox.Draw(spriteBatch, 100, 300);
+            groundItems.Draw();
             base.Draw(gameTime);
         }
 
