7c7645f61c8aa5744f845bbfbb55f78d6b785678
Ugonna <ugowo@outlook.com>
2023-10-12 20:16:22 -0400

    Added Controllers

---
+++ sprint0/Controllers/IController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
+
+public interface IController
+{
+
+    public void registerKeys();
+    public void Update();
+
+
+}
