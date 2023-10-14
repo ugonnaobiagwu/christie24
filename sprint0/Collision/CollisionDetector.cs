5651c5d0972bb935d1dcdcd63f0ad113ed8d751f
Ugonna <ugowo@outlook.com>
2023-10-14 17:22:13 -0400

    Started adding blank / filler methods for the Collision Detector utility class

---
+++ sprint0/Collision/CollisionDetector.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+
+namespace sprint0.Collision
+{
+	public static class CollisionDetector
+	{
+		public enum CollisionType { TOP, BOTTOM, LEFT, RIGHT, NONE }; // Do we need this enum in every Collision Class since this is a utility class?
+
+		/*
+		 * Returns the type of collision that occured on Rectangle b from Rectangle a, if any.
+		 */
+		public static CollisionType CollisionCheck(/* GameObject a, GameObject b*/)
+		{
+			return 0;
+		}
+
+        /*
+		 * Private method used to return exactly where a collision occured.
+		 * 
+		 * DESIGN DETAILS FOR FUTUTE ME WHO HAS YET TO IMPLEMENT:
+		 * -----
+		 * Given that the location member of the generated rectangle gives the Top-left corner of the rectangle.
+		 * This can be used to calculate where in Rectangle b, that Rectangle a collided with Rectangle b.
+		 */
+        private static CollisionType CollisionLocation(Rectangle a, Rectangle b, Rectangle c) {
+			return 0;
+		}
+
+        /*
+		 * Private method used to determine if two rectangles, representing game objects, intersect.
+		 * 
+		 * DESIGN DETAILS FOR FUTUTE ME WHO HAS YET TO IMPLEMENT:
+		 * -----
+		 * Given that the location member of the generated rectangle gives the Top-left corner of the rectangle.
+		 * This can be used to calculate where in Rectangle b, that Rectangle a collided with Rectangle b.
+		 */
+        private static bool CollisionIntersection(Rectangle a, Rectangle b)
+		{
+			return false;
+		}
+
+    }
+}
+
