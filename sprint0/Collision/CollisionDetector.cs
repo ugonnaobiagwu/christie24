76391071488dfc397ea775a68ffc2fa19168ecb0
Ugonna <ugowo@outlook.com>
2023-10-15 18:16:09 -0400

    Continued implementation of the Iteration and Detection utilitty classes

--- sprint0/Collision/CollisionDetector.cs
+++ sprint0/Collision/CollisionDetector.cs
@@ -3,14 +3,28 @@ using Microsoft.Xna.Framework;
 
 namespace sprint0.Collision
 {
-	public static class CollisionDetector
+    /*
+		 * NOTE FOR FUTURE ME, OR ANYONE ELSE HERE. (HI!): 
+		 * Consider changing the protection level of this class?
+		 * 
+		 * The only thing that will/should touch this utility class, 
+		 * and the handler utility, would be the 
+		 * overall iterator, and I'm considering making that a utility 
+		 * class as you don't really need an "instance" of a collision object,
+		 * since all your doing is reporting.
+		 * 
+		 */
+
+    public static class CollisionDetector
 	{
-		public enum CollisionType { TOP, BOTTOM, LEFT, RIGHT, NONE }; // Do we need this enum in every Collision Class since this is a utility class?
+		public enum CollisionType { TOP, BOTTOM, LEFT, RIGHT, NONE };
+		// Do we need this enum in every Collision Class?
 
 		/*
-		 * Returns the type of collision that occured on Rectangle b from Rectangle a, if any.
+		 * Returns the type of collision that occured on Rectangle b from 
+		 * Rectangle a, if any.
 		 */
-		public static CollisionType CollisionCheck(/* GameObject a, GameObject b*/)
+		public static CollisionType CollisionCheck(IGameObject a, IGameObject b)
 		{
 			return 0;
 		}
@@ -20,20 +34,26 @@ namespace sprint0.Collision
 		 * 
 		 * DESIGN DETAILS FOR FUTUTE ME WHO HAS YET TO IMPLEMENT:
 		 * -----
-		 * Given that the location member of the generated rectangle gives the Top-left corner of the rectangle.
-		 * This can be used to calculate where in Rectangle b, that Rectangle a collided with Rectangle b.
+		 * Given that the location member of the generated rectangle gives 
+		 * the Top-left corner of the rectangle.
+		 * 
+		 * This can be used to calculate where in Rectangle b, that 
+		 * Rectangle a collided with Rectangle b.
 		 */
         private static CollisionType CollisionLocation(Rectangle a, Rectangle b, Rectangle c) {
 			return 0;
 		}
 
         /*
-		 * Private method used to determine if two rectangles, representing game objects, intersect.
+		 * Private method used to determine if two rectangles, representing 
+		 * game objects, intersect.
 		 * 
 		 * DESIGN DETAILS FOR FUTUTE ME WHO HAS YET TO IMPLEMENT:
 		 * -----
-		 * Given that the location member of the generated rectangle gives the Top-left corner of the rectangle.
-		 * This can be used to calculate where in Rectangle b, that Rectangle a collided with Rectangle b.
+		 * Given that the location member of the generated rectangle 
+		 * gives the Top-left corner of the rectangle.
+		 * This can be used to calculate where in Rectangle b, 
+		 * that Rectangle a collided with Rectangle b.
 		 */
         private static bool CollisionIntersection(Rectangle a, Rectangle b)
 		{
