17cfddb07502f13ddffddc3e16ccb85611f4fa38
Ugonna <ugowo@outlook.com>
2023-10-15 18:23:25 -0400

    Created GameObject interface

---
+++ sprint0/IGameObject.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace sprint0
+{
+	public interface IGameObject
+	{
+		public int xPosition(); // returns X pos of object
+		public int yPosition(); // returns Y pos of object
+		public int area(); // returns rectangle area of object (i.e.) "how big are you?"
+		public bool isDynamic(); // does this object move? 
+	}
+}
+
