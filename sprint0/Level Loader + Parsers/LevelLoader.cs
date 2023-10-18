fbce7485ae229745b81856582e7a58952ba09f0e
Ryan Greene <greene.1332@buckeyemail.osu.edu>
2023-10-17 21:36:28 -0400

    Began Parser and level loader

---
+++ sprint0/Level Loader + Parsers/LevelLoader.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace sprint0.Level_Loading___Parsers
+{
+    internal static class LevelLoader
+    {
+        /*This will be the function tasked with keeping track of the parser*/
+        public static void CreateRoom(XmlDocument levelFile)
+        {
+
+        }
+        public static void CreateLink()
+        {
+
+        }
+        public static void CreateBlock()
+        {
+
+        }
+        public static void CreateEnemy()
+        {
+
+        }
+    }
+}
---
+++ sprint0/Level Loader + Parsers/XmlParser.cs
@@ -0,0 +1,95 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace sprint0.Level_Loading___Parsers
+{
+    internal static class XmlParser
+    {
+        /*This is the method called in main.
+         An XML Document must be made there and parsed into an XmlNodeList with */
+        public static void ParseFile(XmlDocument doc)
+        {
+            /*Gets a list of each room and iterates through each*/
+            XmlNodeList NodeList = doc.DocumentElement.SelectNodes("Room");
+            foreach (XmlNode node in doc)
+            {
+                ParseRoom(node);
+            }
+
+        }
+        /*Parses every item in the room*/
+        private static void ParseRoom(XmlNode doc)
+        {
+            /*Starts by getting the attribute, tests that it isn't null, and stores it to a variable if it isn't*/
+            var RoomIDAttribute = doc.Attributes["id"];
+            if (RoomIDAttribute != null)
+            {
+                string roomID = RoomIDAttribute.Value;
+            }
+
+            /*Creates the new Xml Node Lists and calls each's parser recursively*/
+            XmlNodeList EnemyNodeList = doc.SelectNodes("Enemy");
+            /*NOTE: Must check before each foreach in case there are none of the specified objects in the room.*/
+            if (EnemyNodeList.Count != 0)
+            {
+                foreach (XmlNode node in EnemyNodeList)
+                {
+                    ParseEnemy(node);
+                }
+            }
+            XmlNodeList LinkNodeList = doc.SelectNodes("Link");
+            if (LinkNodeList.Count != 0)
+            {
+                foreach (XmlNode node in LinkNodeList)
+                {
+                    ParseLink(node);
+                }
+            }
+
+            XmlNodeList BlockNodeList = doc.SelectNodes("Block");
+            if (BlockNodeList.Count != 0)
+            {
+                foreach (XmlElement node in BlockNodeList)
+                {
+                    ParseBlock(node);
+                }
+            }
+            XmlNodeList BoundaryNodeList = doc.SelectNodes("Boundary");
+            if (BoundaryNodeList.Count != 0)
+            {
+                foreach (XmlElement node in BoundaryNodeList)
+                {
+                    ParseBoundary(node);
+                }
+            }
+            XmlNodeList DoorNodeList = doc.SelectNodes("Door");
+            if (DoorNodeList.Count != 0)
+            {
+                foreach (XmlAttribute node in DoorNodeList)
+                {
+                    ParseDoor(node);
+                }
+            }
+
+        }
+
+        private static void ParseLink(XmlNode doc)
+        {
+            /*Creates the parameter for Link's constructor*/
+            XmlNode XNode = doc.SelectSingleNode("x");
+            XmlNode YNode = doc.SelectSingleNode("y");
+            Vector2 Location = new Vector2(float.Parse(XNode.InnerText), float.Parse(YNode.InnerText));
+
+            Type ObjType = typeof(sprint0.Link.Link);
+            ConstructorInfo const = Type.GetConstructor
+
+        }
+    }
+}
