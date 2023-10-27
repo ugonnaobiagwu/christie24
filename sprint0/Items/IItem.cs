506ea8eb19629ec69464f094a466abfe9104b7cc
Ugonna <ugowo@outlook.com>
2023-10-27 17:30:30 -0400

    Boomerangs are done

--- sprint0/Items/IItem.cs
+++ sprint0/Items/IItem.cs
@@ -5,10 +5,7 @@ namespace sprint0
 {
 	public interface IItem
 	{
-		public void Draw(SpriteBatch spriteBatch);
-		public void Update();
 		public void Use(int linkDirection, int linkXPos, int linkYPos);
-		
 	}
 }
 
