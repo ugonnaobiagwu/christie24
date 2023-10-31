643f649f856404ad9b248174fda75273cc049d3d
Ugonna Obiagwu <50896879+ugonnaobiagwu@users.noreply.github.com>
2023-10-30 23:31:52 -0400

    Started Sound System Implementation

---
+++ sprint0/Sound/WindWaker/WindWaker.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.Xna.Framework.Audio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sprint0.Sound.Ocarina
+{
+    /*
+     * NOTE TO SELF: This class is developed in tandem with another class: OCARINA (sfx system)
+     */
+    public static class WindWaker
+    {
+        public enum Songs { ONE, TWO };
+        private static IDictionary<Songs, WindWakerSongData> Library
+            = new Dictionary<Songs, WindWakerSongData>();
+
+        // Returns true or false if the track was loaded into the library.
+        public static bool LoadSong(WindWaker.Songs songName, Songs song)
+        {
+            return false;
+        }
+        // Plays the given track on the assumption that it was properly loaded into the system.
+        // If not, will print an error to console. 
+        public static void PlaySong(WindWaker.Songs songName)
+        {
+
+        }
+        // Stops the given track on the assumption that it was properly loaded into the system.
+        // As well as is currently playing. If not, will print error to console.
+        public static void StopSong(WindWaker.Songs songName)
+        {
+
+        }
+
+        // Pauses the given track on the assumption that it was properly loaded into the system.
+        // As well as is currently playing. If not, will print error to console.
+        public static void PauseSong(WindWaker.Songs songName)
+        {
+
+        }
+
+        // Restarts the given track on the assumption that it was properly loaded into the system.
+        // As well as is currently playing. If not, will print error to console.
+        public static void RestartSong(WindWaker.Songs songName)
+        {
+
+        }
+    }
+}
---
+++ sprint0/Sound/WindWaker/WindWakerSongData.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Media;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sprint0.Sound.Ocarina
+{
+    internal class WindWakerEffectData
+    {
+        public Song song { get; }
+        public bool isLoopable { get; }
+
+        public WindWakerEffectData(Song song, bool isLoopable)
+        {
+            this.song = song;
+            this.isLoopable = isLoopable;
+        }
+    }
+}
