b11e7bcccbf75031f74ee586c596d1cbe1fe5889
XingWuQAQ <144277154+XingWuQAQ@users.noreply.github.com>
2023-11-11 08:08:19 -0500

    Update ScrollState.cs

--- sprint0/GameStates/ScrollState.cs
+++ sprint0/GameStates/ScrollState.cs
@@ -6,5 +6,49 @@ namespace sprint0.GameStates
         public ScrollState()
         {
         }
+        
+        public void ScrollLeft(int currentRoomID, int nextRoomID)
+        {
+            // do logic
+            ScrollTransition("scrollLeft");
+        }
+
+        public void ScrollRight(int currentRoomID, int nextRoomID)
+        {
+            // do logic
+            ScrollTransition("scrollRight");
+        }
+
+        public void ScrollUp(int currentRoomID, int nextRoomID)
+        {
+            // do logic
+            ScrollTransition("scrollRight");
+        }
+
+        public void ScrollDown(int currentRoomID, int nextRoomID)
+        {
+            // do logic
+            ScrollTransition("scrollRight");
+        }
+
+        public void ScrollTransition(string scrollInstruction)
+        {
+            switch (scrollInstruction)
+            {
+                case "scrollLeft":
+                    //logic
+                    break;
+                case "scrollRight":
+                    //logic
+                    break;
+                case "scrollUp":
+                    //logic
+                    break;
+                case "scrollDown":
+                    //logic
+                    break;
+                default: break;
+            }
+        }
     }
 }
